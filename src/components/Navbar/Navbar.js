ad422aa56fd022683c013b92465c3288eddc8513
Ethan Cranston <ecranston@mines.com>
2022-05-11 14:43:46 -0600

    Updated tab bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import { ReactComponent as EthanLogo } from '../../data/EthanLogo1.svg';
 import classNames from "classnames";
--- src/components/Tabbar/Tabbar.js
+++ src/components/Tabbar/Tabbar.js
@@ -3,10 +3,12 @@ import classNames from "classnames";
 import { AiFillHome } from "react-icons/ai";
 import { FaHammer } from "react-icons/fa";
 import { GrMail } from "react-icons/gr"
+import { useNavigate } from "react-router-dom";
 
 import styles from "./Tabbar.module.css";
 
 const Tabbar = ({ navigationData, currentRoute, setCurrentRoute }) => {
+  const navigate = useNavigate();
   const getTabIcon = useCallback((item) => {
     switch (item) {
       case "Home":
@@ -29,7 +31,12 @@ const Tabbar = ({ navigationData, currentRoute, setCurrentRoute }) => {
             styles.tabItem,
             currentRoute === item && styles.tabItemActive,
           ])}
-          onClick={() => setCurrentRoute(item)}
+          onClick={() => {
+            if (window.location.pathname !== '/'){
+              navigate("/")
+            }
+            setCurrentRoute(item)
+          }}
         >
           <span className={styles.icon}>{getTabIcon(item)}</span>
         </span>
