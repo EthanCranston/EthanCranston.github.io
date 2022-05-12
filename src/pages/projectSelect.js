c2445c0ab6bd50ca14309125828b15e29223dfe9
Ethan Cranston <ecranston@mines.com>
2022-05-11 19:06:41 -0600

    flex tiles and gradient

--- src/pages/projectSelect.js
+++ src/pages/projectSelect.js
@@ -1,37 +1,32 @@
 import React from "react";
 import "./Pages.css";
-//import { Link } from "react-router-dom";
+
 import Tile from "../components/Tile/Tile";
+import allProjects from "./projects/projectData";
 
 const ProjectSelect = () => {
+
+    let tiles = [];
+
+    allProjects.forEach(element => {
+        tiles.push(
+            <Tile
+                title={element.title}
+                description={element.description}
+                image={element.image}
+                link={element.link}
+            />)
+    });
+
+
     return (
         <>
-            <h1>Projects</h1>
-            <p>Select a Project!</p>
-
-            <Tile 
-            title={"Crosswords"}
-            description={"A project that takes in arbitry text, a difficult and ouputs a crossword puzzle with hints"}
-            image={"/public/images/data.jpg"}
-            link={"crossword"}
-            />
-
-
-            {/* <ul class="list-disc">
-                <li>one</li>
-                <li>two</li>
-                <li>three</li>
-
-
-
-
-                <li>
-                    <Link to="crossword">Crossword Puzzle Generator</Link> <br></br>
-                </li>
-                <li>
-                    <Link to="puzzlesolver">9x9 Puzzle Solver</Link>
-                </li>
-            </ul> */}
+            <h1 className='text-center'>My Projects:</h1>
+            <div className='flex justify-center'>
+                <div className='flex flex-wrap w-2/3 justify-center m-10  gap-8'>
+                    {tiles}
+                </div>
+            </div>
         </>
     );
 };
