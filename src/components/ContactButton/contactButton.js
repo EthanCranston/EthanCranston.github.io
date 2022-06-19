010516a06a9da429575b5eeaa7e2599a039ddf45
Ethan Cranston <ecranston@mines.com>
2022-06-19 10:36:26 -0600

    updated contact button

--- src/components/ContactButton/contactButton.js
+++ src/components/ContactButton/contactButton.js
@@ -1,27 +1,32 @@
 import React from "react";
 
 const ContactButton = () => {
-    const [showingEmail, setShowingEmail] = React.useState(false);
+    const [showing, setShowing] = React.useState(false);
 
     
-    const nothingToScrapeHere = ['nothingmynothing'.slice(7,9) + 'emxil'.replace('x', 'a'), 'websitexcom'.replace('x','.')]
-
-    const comp = <div>
-        <p>{nothingToScrapeHere.join('@')}</p>
-    </div>
-
-    let email = showingEmail ? comp : (
-        <p
-        onClick={() => setShowingEmail(true)}
+    const nothingToScrapeHere = () => {
+      return ['nothingecnothing'.slice(7,9) + 'rxnston'.replace('x', 'a'), 'minesoedu'.replace('o','.')]
+    }
+    var component;
+    if (showing){
+      component = 
+      <div>
+        <p>{nothingToScrapeHere().join('@')}</p>
+     </div>
+    } else{
+      component =
+      <p
+        onClick={() => setShowing(true)}
         class={"inline-block px-6 py-2.5 bg-yellow-500 text-white rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out"}
       >
-        Click for contact info
+        Click for Details
       </p>
-    );
+    }
+    
     return (
         <React.Suspense fallback={<div>loading...</div>}>
           <div>
-            {email}
+            {component}
           </div>
         </React.Suspense>
       );
--- src/pages/pageControl.js
+++ src/pages/pageControl.js
@@ -1,16 +1,6 @@
 import React, {Suspense} from "react";
-// import HomePage from "./home";
-// import ProjectSelect from "./projectSelect";
-// import ExperiencePage from "./experience";
-// import Page404 from "./page404";
-// import Crossword from "./projects/crossword";
-// import PuzzleSolver from "./projects/puzzleSolver";
-// import DPPathfinder from "./projects/DPPathfinder";
-// import ThisWebsite from "./projects/thisWebsite";
-// import AutoClicker from "./projects/autoClicker";
-// import EquationFinder from "./projects/equationFinder";
-// import Clue from "./projects/clue";
 
+//Use React.lazy to load page only when the user needs it
 const HomePage = React.lazy(() => import("./home"));
 const ProjectSelect = React.lazy(() => import("./projectSelect"));
 const ExperiencePage = React.lazy(() => import("./experience"));
