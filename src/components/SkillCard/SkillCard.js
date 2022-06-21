1a9e5fff5936d5c7f1e6c79ec93643a485de0821
Ethan Cranston <ecranston@mines.com>
2022-06-20 18:54:15 -0600

    more style changes

--- src/components/SkillCard/SkillCard.js
+++ src/components/SkillCard/SkillCard.js
@@ -21,7 +21,7 @@ const EduCard = ({ category, image, skills }) => {
     });
 
     return (
-        <div class="max-w-md m-10 bg-white rounded-xl shadow-md overflow-hidden md:max-w-full border-amber-500 border-b-2">
+        <div class="max-w-md m-10 bg-white rounded-xl shadow-md overflow-hidden md:max-w-full border-gray-500 border-b-2">
             <div class="md:flex">
                 <div class="md:shrink-0">
                     <img src={image} class={"h-48 w-full object-cover md:h-full md:w-48"} alt={category + " graphic"}></img>
