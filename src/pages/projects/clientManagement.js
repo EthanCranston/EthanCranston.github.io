3d48cf0adf69b3a7dedeeb69a765bb5f6deed7cb
Ethan Cranston <ecranston@mines.com>
2022-06-28 19:31:12 -0600

    added more to client managment

--- src/pages/projects/clientManagement.js
+++ src/pages/projects/clientManagement.js
@@ -10,9 +10,17 @@ const CustomerManagement = () => {
           <h1>Client Management System</h1>
           <p>As a software contractor, I developed a polished client management system for
              cyber security evaluations. The software consolidates client data by creating a client profile,
-            providing a graphic user interface to evaluate and score their security practices, then export
+            providing a graphic user interface to evaluate and score their security practices, then exports
             the results into a standardized excel spreadsheet.</p>
-          <CaptionedImage image={scoringGUI} caption="scoring GUI"/>
+          <p>The software can read any set of security standards such as the NIST Cyber Security Framework,
+             or custom requirements. The software can even handle multiple different standards and automatically
+             update related questions between the standards.</p>
+          <CaptionedImage image={scoringGUI} caption="Scoring GUI"/>
+          <p>The client&apos;s compliance can be assessed interactively with the window shown above. In addition
+            to scoring, the GUI allows the user to attach associated files to a question and add notes.
+            Previously-completed compliance data can be imported from excel sheets or from the software&apos;s
+            custom file format.
+          </p>
         </>
   )
 }
